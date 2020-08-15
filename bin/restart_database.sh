dbd05b43c2e6705d5fe3533e012b5f55fddedbb2
Nicolas CARPi <nico-git@deltablot.email>
2020-08-15 06:02:45 +0200

    Add a LICENSE file, a README, and other changes
    * Add a LICENSE file with AGPLv3
    * Add a README with some instructions to get started
    * Move the scripts to `bin/` folder and make them executable
    * Remove questionsNico.md (was empty anyway)

--- bin/restart_database.sh
+++ bin/restart_database.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
 php bin/console cache:clear --env=prod
 php bin/console cache:warmup --env=prod
 php bin/console doctrine:cache:clear-metadata
+php bin/console doctrine:schema:drop --force
+php bin/console doctrine:schema:update --force
